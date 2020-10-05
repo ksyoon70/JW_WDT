4985477f8f897f5a96994533da6a15cf3c72e750
ksyoon70 <ksyoon70@gmail.com>
2020-10-05 13:40:54 +0900

    [시작 M003]  - 프로그램 시작 시 1개만 실행 되도록 수정  - 버전 1.1로 바꿈

--- Jwis_WD/Program.cs
+++ Jwis_WD/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Jwis_WD
 {
@@ -14,9 +15,20 @@ namespace Jwis_WD
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            bool createdNew;
+
+            Mutex mutex = new Mutex(true, "Jwis_WD.exe", out createdNew);
+            if (createdNew)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            else
+            {
+                return;
+            }
+            
         }
     }
 }
